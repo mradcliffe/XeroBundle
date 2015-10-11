ecd9a6a21d756bc9965ffccdfefba31a63bca5ec
Matthew Radcliffe <mradcliffe@softpixel.com>
2015-10-11 00:41:18 -0400

    Add test coverage for Guzzle get request to confirm my poor code.

--- Tests/XeroClientTest.php
+++ Tests/XeroClientTest.php
@@ -3,6 +3,10 @@
 namespace BlackOptic\Bundle\XeroBundle\Tests;
 
 use BlackOptic\Bundle\XeroBundle\XeroClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Exception\RequestException;
 
 class XeroClientTest extends TestBase
 {
@@ -13,7 +17,7 @@ class XeroClientTest extends TestBase
         parent::setUp();
 
         $this->options = array(
-            'base_url' => 'https://api.xero.com/api.xro/2.0',
+            'base_url' => 'https://api.xero.com/api.xro/2.0/',
             'consumer_key' => '',
             'consumer_secret' => '',
             'private_key' => '',
@@ -36,5 +40,26 @@ class XeroClientTest extends TestBase
 
         // Test set token methods.
         $client->setToken('a', 'b');
+    }
+
+    public function testGetRequest() {
+        $mock = new MockHandler(array(
+                new Response(200, array('Content-Length' => 0))
+        ));
+        $this->options['handler'] = HandlerStack::create($mock);
+        $this->options['private_key'] = $this->pemFile;
+
+        $client = new XeroClient($this->options);
+
+        try
+        {
+            $client->get('Accounts');
+        }
+        catch (RequestException $e)
+        {
+            $this->assertNotEquals('401', $e->getCode());
+        }
+
+        $this->assertEquals('/api.xro/2.0/Accounts', $mock->getLastRequest()->getUri()->getPath());
     }
 }
